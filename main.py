ec2725f8a985bb75c45b3aa93174fce5c4ba065b
Andrés Mora <91027622+carlosmora01@users.noreply.github.com>
2023-02-21 10:28:13 -0500

    Actualizado Verificar integridad

--- main.py
+++ main.py
@@ -44,8 +44,11 @@ class Auto:
         registros.add(self.registro)
         registros.add(self.motor.registro)
         for asiento in self.asientos:
-            registros.add(asiento.registro)
-        if len(registros) == 1:
-            return "Auto original"
-        else:
+            if isinstance(asiento, Asiento) and asiento.registro:
+                registros.add(asiento.registro)
+            else:
+                return "Las piezas no son originales"
+        if len(registros) > 1:
             return "Las piezas no son originales"
+        else:
+            return "Auto original"
